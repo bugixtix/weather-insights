51a90f69847401e933cd6faa6e489c6977be39be
Housam_ <ixtix_bug@hotmail.com>
2024-11-25 00:18:10 +0100

    Update: Manipulate and extract date for the next 7 days

--- weather-insights/src/components/WeatherForecast.jsx
+++ weather-insights/src/components/WeatherForecast.jsx
@@ -4,13 +4,16 @@ import React from "react";
 import { WeatherForecast as WeatherForecastStyle, WeatherForecast__box, WeatherForecast__container1, WeatherForecast__label, WeatherForecast__container2 } from "../styles/styles";
 import { OneDayForecast } from "./OneDayForecast";
 
-export default function WeatherForecast({screenWidth}){
+export default function WeatherForecast({screenWidth, _7daysForecast}){
 
-    var dayLong = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wedenesday', 'Thursday','Friday']
-    var dayShort = ['Sat', 'Sun', 'Mon','Tue', 'Wed','Thu', 'Fri']
+    var weekDays = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wedenesday', 'Thursday','Friday']
+    // var dayShort = ['Sat', 'Sun', 'Mon','Tue', 'Wed','Thu', 'Fri']
 
+    function run(){
+        console.log(_7daysForecast)
+    }
     return(
-        <div className="WeatherForecast">
+        <div className="WeatherForecast" onClick={run}>
             <div className="WeatherForecast__box">
 
                 <div className="WeatherForecast__container1" >
@@ -18,7 +21,7 @@ export default function WeatherForecast({screenWidth}){
                 </div>
 
                 <div className="WeatherForecast__container2">
-                    {window.innerWidth > 1024 ? dayLong.map((oneDay,index) => (<OneDayForecast index={index} key={index} day={oneDay}/> )) : dayShort.map((oneDay, index)=>(<OneDayForecast index={index} key={index} day={oneDay}/>))}
+                    {weekDays.map((oneDay,index) => (<OneDayForecast index={index} key={index} day={oneDay}/> ))}
                 </div>
 
             </div>
